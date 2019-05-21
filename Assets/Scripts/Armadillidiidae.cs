cb821074610e800593e711c9aa2cb09d60de4fc3
Yamazaki Mitsuyoshi <mitsuyoshi.tsukuba@gmail.com>
2019-05-21 23:25:25 +0900

    Add standard asset

---
+++ Assets/Scripts/Armadillidiidae.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Armadillidiidae : MonoBehaviour
+{
+    public double direction = 0.0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("I am alive and my name is ");
+
+    }
+}
---
+++ Assets/Scripts/Follow.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Follow : MonoBehaviour
+{
+    public GameObject followee;
+    public double distance = 30.0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.LookAt(followee.transform);
+    }
+}

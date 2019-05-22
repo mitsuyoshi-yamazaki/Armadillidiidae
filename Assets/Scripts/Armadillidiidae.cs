7dae8d3d4f8a7e4c960a21d811a060969f806b3a
Yamazaki Mitsuyoshi <mitsuyoshi.tsukuba@gmail.com>
2019-05-23 02:12:14 +0900

    Update

--- Assets/Scripts/Armadillidiidae.cs
+++ Assets/Scripts/Armadillidiidae.cs
@@ -8,9 +8,14 @@ using UnityEngine;
 
 public class Armadillidiidae : MonoBehaviour
 {
-    public float speed = 0.03f;
-    public float distance = 0.0f;
+    public float speed = 0.01f;
     public Vector3 direction = new Vector3();
+
+    public float fatigure = 0.0f;
+    public float fatigureDecreasement = 0.0008f;
+    // public float drowsiness = 0.0f;
+
+    private RandomWalk randomWalk = new RandomWalk();
 
     // Start is called before the first frame update
     void Start()
@@ -20,11 +25,52 @@ public class Armadillidiidae : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        this.Move();
+        RandomWalk.Output output = this.randomWalk.Run();
+        this.Move(output.direction, output.speed);
+
+        this.fatigure -= this.fatigureDecreasement;
+        this.fatigure = Mathf.Max(this.fatigure, 0f);
+
         this.Animate();
     }
 
-    void Move()
+    private void Move(Vector3 direction, float speed)
+    {
+        float actualSpeed = speed;  // FixMe:
+
+        this.transform.position += actualSpeed * direction;
+
+        this.fatigure += Mathf.Pow(actualSpeed, 2f);
+    }
+
+    private void Animate()
+    {
+        float angle = 120.0f;
+        Vector3 axis = new Vector3(0, 0, 1);
+
+        this.transform.RotateAround(this.transform.position, axis, angle * Time.deltaTime);
+    }
+}
+
+class RandomWalk
+{
+    private float distance = 0.0f;
+    private float speed = 0.03f;
+    private Vector3 direction = new Vector3();
+
+    public struct Output
+    {
+        public float speed;
+        public Vector3 direction;
+
+        public Output(float speed, Vector3 direction)
+        {
+            this.speed = speed;
+            this.direction = direction;
+        }
+    }
+
+    public Output Run()
     {
         if (this.distance <= 0.0f)
         {
@@ -56,16 +102,8 @@ public class Armadillidiidae : MonoBehaviour
             this.distance = Random.Range(1, 5) + Random.Range(1, 5);
         }
 
-
-        this.transform.position += speed * direction;
         this.distance -= speed;
-    }
 
-    void Animate()
-    {
-        float angle = 120.0f;
-        Vector3 axis = new Vector3(0, 0, 1);
-
-        this.transform.RotateAround(this.transform.position, axis, angle * Time.deltaTime);
+        return new Output(this.speed, this.direction);
     }
 }

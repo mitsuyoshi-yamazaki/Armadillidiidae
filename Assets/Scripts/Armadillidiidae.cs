e91e2e0d0a879120542f9248158b7272f16fdea9
Yamazaki Mitsuyoshi <mitsuyoshi.tsukuba@gmail.com>
2019-05-22 23:23:40 +0900

    Remove test script

--- Assets/Scripts/Armadillidiidae.cs
+++ Assets/Scripts/Armadillidiidae.cs
@@ -2,6 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+// Memo
+// https://qiita.com/4_mio_11/items/e7b0a5e65c89ac9d6d7f#こんな時はどうすれば
+// https://www.crossroad-tech.com/entry/Unity_VisualStudioCode4#２The-NET-CLI-Tools-cannot-be-located-が出る
+
 public class Armadillidiidae : MonoBehaviour
 {
     public double direction = 0.0;
@@ -14,7 +18,6 @@ public class Armadillidiidae : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // Debug.Log("I am alive and my name is ");
-
+        transform.position += new Vector3(1, 0, 0);
     }
 }
